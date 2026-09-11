23fe66f16cfce4fc36580c27e4bac2db26a53867
sid0709 <lunalegend990602@gmail.com>
2026-09-10 19:59:44 -0500

    Initial Commit

---
+++ CueTests/CueTests.swift
@@ -0,0 +1,19 @@
+//
+//  CueTests.swift
+//  CueTests
+//
+//  Created by robin on 9/10/26.
+//
+
+import Testing
+@testable import Cue
+
+struct CueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
